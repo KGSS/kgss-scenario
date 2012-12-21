274c17ce2894a5a1eca2c8e8dad682cf5076e70f
Richard Bunt <bunt.rich@googlemail.com>
2012-12-21 22:01:40 +0000

    neutrino part; neutrino scenario file; neutrino module

---
+++ src/NeutrinoSensorModule.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class NeutrinoSensorModule : PartModule
+{
+    [KSPField(guiActive=true, guiName="Value")]
+    string  reading = "";
+
+    public float powerConsumption = 0.05f;
+
+    public override void OnUpdate()
+    {
+        if (getDeployed())
+        {
+            float requiredPower = powerConsumption * TimeWarp.deltaTime;
+            float availPower = part.RequestResource("ElectricCharge", requiredPower);
+
+            if (availPower < requiredPower)
+            {
+                reading = "Not enough power";
+            }
+            else
+            {
+                reading = neutrinoFunction();
+            }
+        }
+        else
+        {
+            reading = "Dish not deployed";
+        }
+    }
+
+    private bool getDeployed()
+    {
+        foreach (var module in part.Modules)
+        {
+            if (module.GetType().Equals(typeof(ModuleAnimateGeneric)))
+            {
+                ModuleAnimateGeneric anim = (ModuleAnimateGeneric)module;
+
+                if (anim.status.Equals("Fixed"))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private string neutrinoFunction()
+    {
+        int neutrinos = 0;
+
+        return neutrinos.ToString() + " neutrinos / s";
+    }
+}
+
